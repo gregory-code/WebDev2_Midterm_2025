a7b39c2967fd2f790e01b71b3d9ab610d38b2611
antoine-b33 <antoinebiegle33@gmail.com>
2025-02-27 17:51:54 -0600

    gameOverlay Sound

--- gameOverlay/main.js
+++ gameOverlay/main.js
@@ -5,31 +5,46 @@ document.addEventListener('DOMContentLoaded', () => {
     const refreshButton = document.getElementById('refreshButton');
     const homeButton = document.getElementById('homeButton');
     const soundToggle = document.getElementById('soundToggle');
+    const buttonAudio = new Audio('../audio/buttonAudio.wav')
 
+    let SoundOn = true;
     let isSoundOn = true;
 
+    function buttonSound() {
+        if (SoundOn) {
+            buttonAudio.currentTime = 0;
+            buttonAudio.play();
+        }
+    }
     document.getElementById("pauseButton").addEventListener("click", function() {
         this.style.display = "none"; 
         document.querySelector(".overlay").classList.add("active"); 
+        buttonSound();
     });
-    
 
-    resumeButton.addEventListener('click', () => { // On click button/ actionable
+    resumeButton.addEventListener('click', () => { 
         overlay.classList.remove('active');
         pauseButton.style.display = 'block'; 
+        buttonSound();
     });
 
-    refreshButton.addEventListener('click', () => {// On click button/ actionable
-        location.reload();
+    refreshButton.addEventListener('click', () => {
+        buttonSound();
+        setTimeout(() => {
+            location.reload();
+        }, 100);
     });
 
-    homeButton.addEventListener('click', () => { // On click button/ acctionable
-        window.location.href = '../index.html';
+    homeButton.addEventListener('click', () => { 
+        buttonSound();
+        setTimeout(() => {
+            window.location.href = '../index.html';
+        }, 100);  
     });
 
     soundToggle.addEventListener('change', () => { // Need some audio :)
-        soundOn = soundToggle.checked;
-        if (SoundOn) {
+        issoundOn = soundToggle.checked;
+        if (isSoundOn) {
             console.log('');
         } else {
             console.log('none');
